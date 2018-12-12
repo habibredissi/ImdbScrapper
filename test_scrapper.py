66c928c22087a22ab2ce21d1e0da3f6f35c9cf89
unknown <habib.redissi@coding-academy.fr>
2018-12-12 14:10:13 +0100

    Python

--- test_scrapper.py
+++ test_scrapper.py
@@ -4,11 +4,6 @@ import scrapper
 
 class TestScrap(unittest.TestCase):
 
-    # def test_add(self):
-    #     self.assertEqual(t.add(10, 5), 15)
-    #     self.assertEqual(t.add(-1, 1), 0)
-    #     self.assertEqual(t.add(-1, -1), -2)
-
     def test_importTopMovies(self):
         try:
             scrapper.importTopMovies()
